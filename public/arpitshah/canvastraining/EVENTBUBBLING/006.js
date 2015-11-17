4b904f1e3652dd5ba56ed17234ac97bd3e57eba9
Arpit Shah <aarpit@live.com>
2015-11-17 16:34:30 -0500

    no message

---
+++ public/arpitshah/canvastraining/EVENTBUBBLING/006.js
@@ -0,0 +1,12 @@
+document.getElementById("Facebook").addEventListener("click", function(){
+    window.location.assign("https://www.facebook.com/");
+});
+document.getElementById("Google").addEventListener("click", function(){
+    window.location.assign("https://www.google.com/?gws_rd=ssl");
+});
+document.getElementById("Twitter").addEventListener("click", function(){
+    window.location.assign("https://twitter.com/?lang=en");
+});
+document.getElementById("YouTube").addEventListener("click", function(){
+    window.location.assign("https://www.youtube.com/");
+});
