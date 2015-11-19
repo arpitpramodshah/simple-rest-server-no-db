cea581ee662ff6f4859e187c1f28087ad8389662
Arpit Shah <aarpit@live.com>
2015-11-19 12:12:46 -0500

    APPLE

---
+++ public/arpitshah/canvastraining/APPLE/0005.js
@@ -0,0 +1,22 @@
+var fruitName = document.getElementById('myFruit').children;
+for(i=0; i<fruitName.length; i++){
+	fruitName[i].addEventListener("click", function(){
+		if(this.checked){
+			var createInput = document.createElement("div");
+			var newTextValue = document.createTextNode(this.value);
+			var finalCart = createInput.appendChild(newTextValue);
+			var finalCartId = document.getElementById("myFruitCart");
+			var finalCartFruit = finalCartId.appendChild(createInput);
+			//document.getElementById("myFruitCart").innerHTML +=  + "</br>";
+		} else if (this.checked == false){
+			var FinalFruit = document.getElementsByTagName("div");
+        		for(var i=0;i<FinalFruit.length;i++) {
+            		if(this.value === FinalFruit[i].innerHTML) {
+                		document.getElementById("myFruitCart").removeChild(FinalFruit[i]);
+						//document.getElementById("myFruitCart").innerHTML = null;
+		}	
+	}
+}
+})
+}
+
