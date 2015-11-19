09d0343c43bb2aa345a560e0937872a7589f3136
Arpit Shah <aarpit@live.com>
2015-11-19 14:01:42 -0500

    no message

--- public/arpitshah/canvastraining/APPLE_NEXTSIBLING/007.js
+++ public/arpitshah/canvastraining/APPLE_NEXTSIBLING/007.js
@@ -3,20 +3,16 @@ for(i=0; i<fruitName.length; i++){
 	fruitName[i].addEventListener("click", function(){
 		if(this.checked){
 			var createInput = document.createElement("div");
-			//console.log(createInput);
-			var newTextValue = document.createTextNode(this.value);
-			//console.log(newTextValue);
+			var newTextValue = document.createTextNode(this.nextSibling.innerHTML);
+				//console.log(newTextValue);
 			var finalCart = createInput.appendChild(newTextValue);
-			//console.log(finalCart);
 			var finalCartId = document.getElementById("myFruitCart");
-			//console.log(finalCartId);
 			var finalCartFruit = finalCartId.appendChild(createInput);
-			//console.log(finalCartFruit);
 			//document.getElementById("myFruitCart").innerHTML +=  + "</br>";
 		} else if (this.checked == false){
 			var FinalFruit = document.getElementsByTagName("div");
         		for(var i=0;i<FinalFruit.length;i++) {
-            		if(this.value === FinalFruit[i].innerHTML) {
+            		if(this.nextSibling.innerHTML === FinalFruit[i].innerHTML) {
                 		document.getElementById("myFruitCart").removeChild(FinalFruit[i]);
 						//document.getElementById("myFruitCart").innerHTML = null;
 		}	
