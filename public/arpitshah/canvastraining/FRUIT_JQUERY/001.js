2335e0f9ffd45c0b59b56a68a2accd54a319505a
Arpit Shah <aarpit@live.com>
2015-12-03 11:26:16 -0500

    no message

--- public/arpitshah/canvastraining/FRUIT_JQUERY/001.js
+++ public/arpitshah/canvastraining/FRUIT_JQUERY/001.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $("#myFruit").on("change", 'input[type="checkbox"]', function() {
         var $chkBoxFruit = $(this);
-        //console.log($chkBoxFruit);
+        //console.log(this);
         if ($chkBoxFruit.is(':checked')) {
             var fruitValue = $(this).attr('id');
             $("#myFruitCart").append("<div>" + fruitValue + "</div>");
@@ -10,15 +10,41 @@ $(document).ready(function() {
             //console.log(removeFruitArray);
             for (var i = 0; i < removeFruitArray.length; i++) {
                 //console.log(removeFruitArray[i].innerHTML);
-                if (this.id == removeFruitArray[i].innerHTML) {
+                if (this.id === removeFruitArray[i].innerHTML) {
                     $(removeFruitArray[i]).remove();
+                    //console.log(myFruitCart.innerHTML);
                 }
             }
         }
     });
-    $("#addFruit").on("click", function(){
+
+    $("#myFruit").on("click", 'span', function() {
+        var $chkBoxFruitSpan = $(this).html();
+        var $previousCheckBox = $(this).prev();
+        console.log($previousCheckBox.prop("checked"));
+        if ($previousCheckBox.prop("checked") == false) {
+            console.log('hello');
+            $previousCheckBox.prop("checked", true);
+            $("#myFruitCart").append("<div>" + $chkBoxFruitSpan + "</div>");
+        } else if ($previousCheckBox.prop("checked") == true) {
+            console.log('hi');
+            $previousCheckBox.prop("checked", false);
+            var removeFruitArraySpan = $('#myFruitCart').find('div');
+            for (var j = 0; j < removeFruitArraySpan.length; j++) {
+                if ($(this).html() === removeFruitArraySpan[j].innerHTML) {
+                    $(removeFruitArraySpan[j]).remove();
+                }
+            }
+        }
+    });
+
+    $("#addFruit").on("click", function() {
         var userFruit = prompt("Please enter your choice of fruit");
-        var result = $('<input/>', {type: 'checkbox', id: userFruit, name: "fruit"}).appendTo("#myFruit");
-        $("#myFruit").append(userFruit);
+        var result = $('<input/>', {
+            type: 'checkbox',
+            id: userFruit,
+            name: "fruit"
+        }).appendTo("#myFruit");
+        $("#myFruit").append("<span>" + userFruit + "</span>");
     });
 });
